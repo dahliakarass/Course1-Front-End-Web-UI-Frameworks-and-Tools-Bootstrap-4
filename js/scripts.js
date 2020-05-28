dd6ee9cde8fe853185a114f3b0ceb28dfb29918d
Dahlia Karass <dahliamagdi@gmail.com>
2020-05-28 03:41:06 +0200

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+            $("#carouselButton").removeClass('btn-danger');
+            $("#carouselButton").addClass('btn-success');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+            $("#carouselButton").removeClass('btn-success');
+            $("#carouselButton").addClass('btn-danger');
+        }
+    });
+    $("#LoginButton").click(function(){
+        $('#LoginModal').modal('toggle'); 
+    });
+
+    $("#ReserveButton").click(function(){
+        $('#ReservationModal').modal('toggle'); 
+    });
+
+    //TOOLTIP
+    // $(document).ready(function(){
+    //     $('[data-toggle="tooltip"]').tooltip();
+    // });
+
+});
