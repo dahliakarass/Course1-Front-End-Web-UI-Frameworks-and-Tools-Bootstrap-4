60edb46f20f8ae1226206c1322465362b135a2e3
Dahlia Karass <dahliamagdi@gmail.com>
2020-06-08 20:50:58 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+console.log('script.js says "I\'m here"');
+
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("span").hasClass('fa-pause')) {
             $("#mycarousel").carousel('pause');
             $("#carouselButton").children("span").removeClass('fa-pause');
@@ -8,25 +10,22 @@ $(document).ready(function(){
             $("#carouselButton").removeClass('btn-danger');
             $("#carouselButton").addClass('btn-success');
         }
-        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
             $("#mycarousel").carousel('cycle');
             $("#carouselButton").children("span").removeClass('fa-play');
-            $("#carouselButton").children("span").addClass('fa-pause');                    
+            $("#carouselButton").children("span").addClass('fa-pause');
             $("#carouselButton").removeClass('btn-success');
             $("#carouselButton").addClass('btn-danger');
         }
     });
-    $("#LoginButton").click(function(){
-        $('#LoginModal').modal('toggle'); 
+    $("#LoginButton").click(function () {
+        $('#LoginModal').modal('toggle');
     });
-
-    $("#ReserveButton").click(function(){
-        $('#ReservationModal').modal('toggle'); 
+    $("#ReserveButton").click(function () {
+        $('#ReservationModal').modal('toggle');
     });
-
-    //TOOLTIP
     // $(document).ready(function(){
     //     $('[data-toggle="tooltip"]').tooltip();
     // });
-
 });
+
